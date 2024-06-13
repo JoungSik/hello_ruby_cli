f825ed4a4a491ec314e76ef8a603814c8d26a56f
JoungSik <tjstlr2010@gmail.com>
2024-06-13 18:49:55 +0900

    deploy 0.1.0

--- lib/hello_ruby_cli.rb
+++ lib/hello_ruby_cli.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
 require_relative 'hello_ruby_cli/version'
+require 'thor'
 
 module HelloRubyCli
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Main Cli Class
+  class CLI < Thor
+    desc 'hello [name]', 'say my name'
+
+    def hello(name)
+      if name == 'joungsik'
+        puts 'you are goddamn right'
+      else
+        puts 'say my name'
+      end
+    end
+  end
 end
